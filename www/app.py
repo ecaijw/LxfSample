6524f565fe8175f8085909a7281708cb8dd2feec
Jingwei Cai <caijingwei@MI-20170323JTIF.(none)>
2017-09-30 17:59:21 +0800

    2.7 python app.py

--- www/app.py
+++ www/app.py
@@ -1,21 +1,25 @@
-import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
-from datetime import datetime
+from flask import Flask
+from flask import request
 
-from aiohttp import web
+app = Flask(__name__)
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return "<h1>Welcome to ecaijw's Home</h1>"
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+@app.route('/signin', methods=['POST'])
+def signin():
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
